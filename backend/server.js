f43a194eac03dafff24d9fb2c285567003e9f603
hitesh-hr08 <hitesh-hr08@users.noreply.github.com>
2026-06-13 08:56:50 +0530

    Fix static serving for CSS/JS/images

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "frontend")));
+app.use(express.static(path.join(__dirname, "..")));
 
 app.get("/", (req, res) => {
   res.redirect("/home.html");
